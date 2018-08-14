d02ef732d61b86c4259a4582e5fabfdae10cff14
Edvan Macedo <edvan.macedo.jr@gmail.com>
2018-08-14 22:27:38 +0200

    Simple axios API call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '7e8fd7b287e97eca0aee4472c87d6f9b';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+
+}
